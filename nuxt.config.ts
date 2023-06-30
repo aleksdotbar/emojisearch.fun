a00bc413b4c43598da24934e9923808c548e7445
Alexander B <mail@xanderbarkhatov.com>
2023-06-30 19:14:32 +0600

    feat: analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxthq/ui", "@vueuse/nuxt", "@nuxtjs/fontaine", "@nuxtjs/google-fonts"],
+  modules: [
+    "@nuxthq/ui",
+    "@vueuse/nuxt",
+    "@nuxtjs/fontaine",
+    "@nuxtjs/google-fonts",
+    "nuxt-vercel-analytics",
+  ],
   runtimeConfig: {
     openaiApiKey: "",
   },
